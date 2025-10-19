0d7583bcc01c607aed64d47399bf515d7bee58e6
gabiesoares <gabriellexslima@gmail.com>
2025-10-19 12:51:34 +0000

    Finalização do Código - Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,75 @@
 #include <stdio.h>
 
+//criacao da funcao de movimentacao da torre (5 casas, direita) 
+void moverTorre(int torre){
+    if(torre > 0){
+        printf("Direita\n");
+        moverTorre (torre - 1);
+    }
+}
+
+//criacao da funcao de movimentacao do bispo (5 casas, diagonal superior direita) 
+void moverBispo(int bispoVer, int bispoHor){
+    for (int i = 1; i <= bispoVer; i++) {        //movimento vertical
+        printf("Cima\n");
+        for (int j = 1; j <= bispoHor; j++) {    //movimento horizontal
+            if (j == i) {                          
+                printf("Direita\n\n");
+                break;                             
+            }
+        }
+    }
+}
+
+//criacao da funcao de movimentacao da rainha (8 casas, esquerda) 
+void moverRainha(int rainha){
+    if(rainha > 0){
+        printf("Esquerda\n");
+        moverRainha (rainha - 1);
+    }
+}
+
 int main(){
 
-    int i = 1, j = 1, k = 1;
+    int i = 1, j = 1;
 
     printf("***XADREZ DA ESTÁCIO***\n");
     
-    //movimentacao bispo (5 casas, diagonal superior direita) utilizando while
-    printf("\nMOVIMENTACAO DO BISPO:\n");
+    //movimentacao recursiva do bispo
+    printf("\nMOVIMENTACAO DO BISPO:\n\n");
+    moverBispo(5, 5);
 
-    while (i <= 5){
-        printf("\nQuantidade: %i\nMovimentação: Cima, Direita\n", i);
-        i++;
-    }
-
-    //movimentacao torre (5 casas, direita) utilizando do-while
-    i = 1;
-    printf("\nMOVIMENTACAO DA TORRE:\n");
-    do{
-        printf("\nQuantidade: %i\nMovimentação: Direita\n", i); 
-        i++;
-    } while (i <= 5);
+    //movimentacao recursiva da torre
+    printf("\nMOVIMENTACAO DA TORRE:\n\n");
+    moverTorre(5);
     
-    //movimentacao rainha (8 casas,esquerda) utilizando for
-    printf("\nMOVIMENTACAO DA RAINHA\n");
-    for (i = 1; i <= 8; i++){
-        printf("\nQuantidade: %i\nMovimentação: Esquerda\n", i);
-    }
+    //movimentacao recursiva da rainha
+    printf("\nMOVIMENTACAO DA RAINHA\n\n");
+    moverRainha(8);
 
-    printf("\nMOVIMENTACAO DO CAVALO\n");
-    for (i = 1; i <= 2; i++){
-        while(j <= 2){
-                printf("\nQuantidade: %i\nMovimentação: Baixo\n", j);
-                j++;
-            }
+    //movimentacao do cavalo (2 casas, cima e 1 casa, direita) utilizando for e while
+    printf("\nMOVIMENTACAO DO CAVALO\n\n");
+    
+    int moveL = 3;
+    for (i = 1; i <= moveL; i++) {
+
+        //movimento vertical
+        j = 0;
+        while (j < 2) {
+            printf("Cima\n");
+            j++;
+            if (j >= 2) break;  // garante que não passe de 2 casas
         }
-        
-    do{
-        printf("\nQuantidade: %i\nMovimentação: Esquerda\n", k);
-        k++;
-    } while (k <= 1);
 
+        // movimento horizontal
+        j = 0;
+        while (j < 1) {
+            printf("Direita\n\n");
+            j++;
+            if (j >= 1) break;  // garante que não passe de 1 casa
+        }
+    }
+    
     printf("\nMovimentacao cessada! Programa encerrado.");
 
     return 0;
